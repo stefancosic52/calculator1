d298d805884966db5adf5eede3ebc29672edcb02
stefancosic52@outlook.com <stefancosic52@outlook.com>
2021-05-10 11:03:02 +0200

    ok

---
+++ calculator1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double prvi = Convert.ToDouble(textBox1.Text);
+            double drugi = Convert.ToDouble(textBox2.Text);
+
+            double sum = prvi + drugi;
+
+            textBox3.Text = sum.ToString();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
